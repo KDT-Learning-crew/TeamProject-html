e3951e6ed6a99f153e23b5748395d262aa75248c
snaw0110 <sn3188@naver.com>
2025-01-11 02:16:30 +0900

    250111 마이페이지 메인 부분완성

---
+++ workspace/webapp/assets/js/myPageMain/myPageMain.js
@@ -0,0 +1,11 @@
+const myPagePhoneBtn = document.querySelector(".myPage-phoneBtn");
+const myPagePhoneCheck = document.querySelector(".myPage-phoneCheck");
+console.log(myPagePhoneBtn);
+
+
+
+
+myPagePhoneBtn.addEventListener("click", () => {
+   myPagePhoneCheck.style = "display:block"
+});
+
